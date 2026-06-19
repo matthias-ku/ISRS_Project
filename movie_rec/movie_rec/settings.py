a1b61f7f35b1ed62cd835fa88772fdd0dd02ccfb
Joanna Walczak <joasia.walczak@gmail.com>
2026-06-19 19:32:33 +0200

    Overall UI improvement

--- movie_rec/movie_rec/settings.py
+++ movie_rec/movie_rec/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TMDB_API_KEY = os.environ.get("TMDB_API_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
